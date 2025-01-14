c959e792fadb5d81ae8501f9dc32df9e2c4bb07f
Patricia Gagnon-Renaud <pgrenaud@gosecure.net>
2025-01-14 17:23:18 -0500

    WIP

--- reconftw.sh
+++ reconftw.sh
@@ -6057,6 +6057,14 @@ if [[ $OSTYPE == "darwin"* ]]; then
 		printf "\n%bBrew is not installed or not in the PATH.%b\n\n" "$bred" "$reset"
 		exit 1
 	fi
+	if [[ -x "$(brew --prefix gnu-getopt)/bin/getopt" ]]; then
+		printf "\n%bBrew forumlae gnu-getopt is not installed.%b\n\n" "$bred" "$reset"
+		exit 1
+	fi
+	if [[ -d "$(brew --prefix coreutils)/libexec/gnubin" ]]; then
+		printf "\n%bBrew forumlae coreutils is not installed.%b\n\n" "$bred" "$reset"
+		exit 1
+	fi
 	# Prefix is different depending on Intel vs Apple Silicon
 	PATH="$(brew --prefix gnu-getopt)/bin:$PATH"
 	PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
